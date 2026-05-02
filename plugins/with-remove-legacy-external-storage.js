e45181b126f0c82ac1ada10894a0f4fdcb1a837c
Tom Reed <trucktapteam@gmail.com>
2026-05-02 17:54:43 -0400

    Remove broad Android media permissions

--- plugins/with-remove-legacy-external-storage.js
+++ plugins/with-remove-legacy-external-storage.js
@@ -1,7 +1,35 @@
 const { AndroidConfig, withAndroidManifest } = require('expo/config-plugins');
 
+const BLOCKED_MEDIA_PERMISSIONS = new Set([
+  'android.permission.READ_EXTERNAL_STORAGE',
+  'android.permission.WRITE_EXTERNAL_STORAGE',
+  'android.permission.READ_MEDIA_IMAGES',
+  'android.permission.READ_MEDIA_VIDEO',
+  'android.permission.READ_MEDIA_AUDIO',
+  'android.permission.ACCESS_MEDIA_LOCATION',
+]);
+
+function removeBlockedPermissions(manifest, key) {
+  const permissions = manifest[key];
+  if (!Array.isArray(permissions)) {
+    return;
+  }
+
+  manifest[key] = permissions.filter((permission) => {
+    const name = permission?.$?.['android:name'];
+    return !BLOCKED_MEDIA_PERMISSIONS.has(name);
+  });
+
+  if (manifest[key].length === 0) {
+    delete manifest[key];
+  }
+}
+
 module.exports = function withRemoveLegacyExternalStorage(config) {
   return withAndroidManifest(config, (config) => {
+    removeBlockedPermissions(config.modResults.manifest, 'uses-permission');
+    removeBlockedPermissions(config.modResults.manifest, 'uses-permission-sdk-23');
+
     const application = AndroidConfig.Manifest.getMainApplication(config.modResults);
 
     if (application?.$) {
